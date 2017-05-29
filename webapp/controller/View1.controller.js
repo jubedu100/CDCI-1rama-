d1f1fc6d96fc99b92de30e0580f2138db3edcae9
Julian BenÃ­tez DurÃ¡n <jubedu100@gmail.com>
2017-05-29 11:08:40 +0000

    Filtro aplicado

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,29 +6,32 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("CDCI1.controller.View1", {
-	handleSelectionFinish: 	function(oEvent) {
-		/*var lv_refresh = false;*/
-			/*console("value:"+this.getSelectedKeys());},*/
-	  var oSelectedItem = oEvent.getParameter("selectedItems");
-	  
-	   var sQuery = [];
-	   for (var i = 0; i < oSelectedItem.length; i++ ){
-	   	sQuery.push(oSelectedItem[i].sId);
-	   	
-	   }
-	   /*oSelectedItem[0].sId;*/
-    var aCurrentFilterValues = [];
-
-			/*aCurrentFilterValues.push(this.getSelectedItemText(sQuery));*/
-			/*aCurrentFilterValues.push(this.getSelectedItemText(this.oSelectCategory));
-			aCurrentFilterValues.push(this.getSelectedItemText(this.oSelectSupplierName));
-
-			this.filterTable(aCurrentFilterValues);*/
+		handleSelectionFinish: function(oEvent) {
 			
-			var oColumn = oEvent.getParameter("column");
+			//implementacion del filtro
+			
+			//recuperamos los Items seleccionados
+			var oSelectedItem = oEvent.getParameter("selectedItems");
+			
+			//definimos el array dónde se van a definir los parámetros del filtro
+			var aFilter = [];
+			
+			//definimos la variable donde se va a guardar el text o key del Item seleccionado
+			var sQuery;
 
-    }
+			//cargamos el filtro con los parámetros a filtrar
+			for (var i = 0; i < oSelectedItem.length; i++) {
 
+				sQuery = oSelectedItem[i].mProperties.key;
+				aFilter.push(new Filter("Carrid", FilterOperator.EQ, sQuery));
+			}
+			
+			//recuperamos los datos de la tabla
+			var oTable = this.getView().byId("Vuelos");
+			var oBinding = oTable.getBinding("rows");
+			
+			//aplicamos el filtro
+			oBinding.filter(aFilter);
+		}
 	});
-	
 });
