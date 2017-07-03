9ef43683d4f959c15a40167d793b2785666ff610
Victor Del  Dedo Gonzalez <victor.deldedo@unisys.com>
2017-07-03 14:24:32 +0000

    update ok

--- webapp/controller/view2.controller.js
+++ webapp/controller/view2.controller.js
@@ -7,7 +7,10 @@ sap.ui.define([
 	'sap/ui/model/FilterOperator',
 	'sap/ui/core/format/DateFormat',
 	'sap/ui/model/Sorter',
-	'sap/ui/table/SortOrder'
+	'sap/ui/table/SortOrder',
+	'sap/ui/unified/FileUploaderParameter'
+
+	
 
 ], function(jQuery, MessageToast, UploadCollectionParameter, Controller, Filter, FilterOperator, DateFormat, Sorter, SortOrder) {
 	"use strict";
@@ -70,149 +73,97 @@ sap.ui.define([
 
 		/*	var PageController = Controller.extend("sap.m.sample.UploadCollectionForPendingUpload.Page", {*/
 
-		onChange: function(oEvent) {
+		onChange : function(oEvent) {
+
 			var oUploadCollection = oEvent.getSource();
-			// Header Token
+			
 			var oCustomerHeaderToken = new UploadCollectionParameter({
-				name: "x-csrf-token",
-				value: "securityTokenFromModel"
+				name : "x-csrf-token",
+				value : "securityTokenFromModel"
+				
 			});
 			oUploadCollection.addHeaderParameter(oCustomerHeaderToken);
 			MessageToast.show("Event change triggered");
+			
 		},
-
-		onFileDeleted: function(oEvent) {
+ 
+		onFileDeleted : function(oEvent) {
 			MessageToast.show("Event fileDeleted triggered");
 		},
-
-		onFilenameLengthExceed: function(oEvent) {
+ 
+		onFilenameLengthExceed : function(oEvent) {
 			MessageToast.show("Event filenameLengthExceed triggered");
 		},
-
-		onFileSizeExceed: function(oEvent) {
+ 
+		onFileSizeExceed : function(oEvent) {
 			MessageToast.show("Event fileSizeExceed triggered");
 		},
-
-		onTypeMissmatch: function(oEvent) {
+ 
+		onTypeMissmatch : function(oEvent) {
 			MessageToast.show("Event typeMissmatch triggered");
 		},
-
-		onStartUpload: function(oEvent) {
-			/*var oUploadCollection = this.getView().byId("UploadCollection");*/
-			/*			var a = oUploadCollection.mProperties.uploadUrl();*/
-			/*			var oTextArea = this.getView().byId("TextArea");*/
-			/*			var cFiles = oUploadCollection.getItems().length;
-						var uploadInfo = "";
-
-						oUploadCollection.upload();
-
-						uploadInfo = cFiles + " file(s)";*/
-			/*			if (oTextArea.getValue().length === 0) {
-							uploadInfo = uploadInfo + " without notes";
-						} else {
-							uploadInfo = uploadInfo + " with notes";
-						}*/
-
-			/*			MessageToast.show("Method Upload is called (" + uploadInfo + ")");
-						sap.m.MessageBox.information("Uploaded " + uploadInfo);*/
-			/*			oTextArea.setValue("");*/
-
-			var url1 = "/sflight/sap/opu/odata/SAP/Z001_VUELOS_PRUEBA_SRV/FileSet('prueba.docx')/$value";
-			var csrfToken = "";
-			var oFileUploader1 = new sap.ui.commons.FileUploader();
+ 
+		onStartUpload : function(oEvent) {
+			var oUploadCollection = this.getView().byId("UploadCollection");
+		/*	var oTextArea = this.getView().byId("TextArea");*/
+			var cFiles = oUploadCollection.getItems().length;
+			var uploadInfo = "";
+			// The call
 			
-			var aData = jQuery.ajax({
-				url: url1,
-				username: "ZUNISYS01",
-				password: "ZUNISYS01",
-
-				headers: {
-					"X-CSRF-Token": "Fetch",
-					"X-Requested-With": "XMLHttpRequest",
-					"Content-Type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-					"DataServiceVersion": "2.0"
-				},
-				type: "PUT",
-				jsonpCallback: 'putJSON',
-				contentType: "application/json",
-				dataType: "json",
-
-				//data : “”,response
-				success: function(data, textStatus, jqXHR) {
-
-					csrfToken = jqXHR.getResponseHeader('x-csrf-token');
-
-					oFileUploader1.setCsrf(csrfToken);
-					oFileUploader1.setSlug("2000006");
-
-					oFileUploader1.setUploadUrl("/sflight/sap/opu/odata/SAP/Z001_VUELOS_PRUEBA_SRV/FileSet/('prueba.docx')/$value");
-
-					oFileUploader1.upload();
-
-				},
-				error: function(XMLHttpRequest, textStatus, errorThrown) {
-
-				}
-
-			});
-
+			oUploadCollection.upload();
+ 
+			uploadInfo = cFiles + " file(s)";
+/*			if (oTextArea.getValue().length === 0) {
+				uploadInfo = uploadInfo + " without notes";
+			} else {
+				uploadInfo = uploadInfo + " with notes";
+			}*/
+ 
+			MessageToast.show("Method Upload is called (" + uploadInfo + ")");
+			sap.m.MessageBox.information("Uploaded " + uploadInfo);
+		/*	oTextArea.setValue("");*/
 		},
-
-		onBeforeUploadStarts: function(oEvent) {
+ 
+		onBeforeUploadStarts : function(oEvent) {
 			// Header Slug
 			var oCustomerHeaderSlug = new sap.m.UploadCollectionParameter({
-				name: "slug",
-				value: oEvent.getParameter("fileName")
+				name : "slug",
+				value : oEvent.getParameter("fileName")
 			});
 			oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug);
+			
+			var oCustomerHeaderSlug1 = new sap.m.UploadCollectionParameter({
+				name : "X-Requested-With",
+				value : "XMLHttpRequest"
+			});
+			oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug1);
+			
+			
 			setTimeout(function() {
 				MessageToast.show("Event beforeUploadStarts triggered");
 			}, 4000);
 		},
-
-		onUploadComplete: function(oEvent) {
-			/*			var sUploadedFile = oEvent.getParameter("files")[0].fileName;
-						var location = "/sap/opu/odata/SAP/Z001_VUELOS_PRUEBA_SRV";
-						var index = location.indexOf("/FileSet");
-						var path = location.substring(index);
-						var oCollection = oEvent.getSource();
-						var collectionPath = "/FileSet('" + sUploadedFile + "')/$value";
-						
-						var oTemplate = this.byId(collectionPath).clone();
-						
-						collectionPath = oTemplate.getId();
-						oCollection.bindAggregation("items", {
-							path: collectionPath,
-							template: oTemplate
-						});*/
-			/*			setTimeout(function() {
-							_busyDialog.close();
-							MessageToast.show(_oBundle.getText("dokumentHochgeladen"));
-						}, 3000);*/
+ 
+		onUploadComplete : function(oEvent) {
 
 			var sUploadedFileName = oEvent.getParameter("files")[0].fileName;
 			setTimeout(function() {
 				var oUploadCollection = this.getView().byId("UploadCollection");
-
+ 
 				for (var i = 0; i < oUploadCollection.getItems().length; i++) {
 					if (oUploadCollection.getItems()[i].getFileName() === sUploadedFileName) {
 						oUploadCollection.removeItem(oUploadCollection.getItems()[i]);
 						break;
 					}
 				}
-
 				// delay the success message in order to see other messages before
 				MessageToast.show("Event uploadComplete triggered");
 			}.bind(this), 8000);
 		},
-
-		onSelectChange: function(oEvent) {
+ 
+		onSelectChange : function(oEvent) {
 			var oUploadCollection = this.getView().byId("UploadCollection");
 			oUploadCollection.setShowSeparators(oEvent.getParameters().selectedItem.getProperty("key"));
 		}
-
-		/*	});*/
-
 	});
-
 });
